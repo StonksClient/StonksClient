7edfe9aeafc245ed2ed66a9d9b959bd487f75712
timoreo <timo.oreo34@gmail.com>
2021-01-12 12:09:18 +0100

    First commit : basic start files

---
+++ src/main/java/com/stonks/stonksclient/StonksClient.java
@@ -0,0 +1,10 @@
+package com.stonks.stonksclient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StonksClient implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
